afd731dae1c1bf949240d221ed1db8b4385a8b3a
Romer Girginer <girgineromer3019@gmail.com>
2026-02-18 12:00:43 +0100

    CurrencyApp
    I fixed currency-app

--- src/component/Currency.jsx
+++ src/component/Currency.jsx
@@ -1,19 +1,54 @@
 import './Currency.css'
 import { FaMoneyBillTransfer } from "react-icons/fa6";
+import { useState } from 'react';
+import axios from 'axios';
+import photo from './para.png';
 
 function Currency() {
+
+    const [amount, setAmount] = useState('0');
+    const [fromCurrency, setFromCurrency] = useState('EUR');
+    const [toCurrency, setToCurrency] = useState('TRY');
+    const [result, setResult] = useState('0');
+
+    let BaseURL = `https://open.er-api.com/v6/latest/${fromCurrency}`;
+    let API_Key = 'fca_live_75E9ncW4luUwsN94YiFagfnWc2XxGn90uxlJye3D';
+
+    let EXCHANGE_API = `${BaseURL}?base=${fromCurrency}&symbols=${toCurrency}`;
+
+
+    const exchange = async () => {
+    try {
+        const response = await axios.get(BaseURL);
+
+        const rate = response.data.rates[toCurrency];
+        const calculated = (amount * rate).toFixed(2);
+
+        setResult(calculated);
+    } catch (error) {
+        console.log("Hata oluştu:", error);
+    }
+}
+
+
   return (
     <div className="currency-div">
+        <div><h2 className="title">Currency Conversion</h2></div>
+      <input 
+        value={amount} 
+        onChange={(e) => setAmount(e.target.value)} 
+        type="number" 
+        className="amount" 
+        placeholder="Miktar" 
+      />
 
-      <h2 className="title">Currency Conversion</h2>
-
-      <input type="number" className="amount" placeholder="Miktar" />
-
-      <select className="from-currency-amount">
-        <option value="EUR">EUR</option>
+      <select 
+        value={fromCurrency} 
+        onChange={(e) => setFromCurrency(e.target.value)} 
+        className="from-currency-amount"
+      >
         <option value="USD">USD</option>
-        <option value="GBP">GBP</option>
-        <option value="JPY">JPY</option>
+        <option value="EUR">EUR</option>
         <option value="TRY">TRY</option>
       </select>
 
@@ -21,22 +56,32 @@ function Currency() {
         <FaMoneyBillTransfer className="transfer-icon" />
       </div>
 
-      <select className="to-currency-amount">
+      <select 
+        value={toCurrency} 
+        onChange={(e) => setToCurrency(e.target.value)} 
+        className="to-currency-amount"
+      >
         <option value="TRY">TRY</option>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
-        <option value="GBP">GBP</option>
-        <option value="JPY">JPY</option>
       </select>
 
-      <input type="number" className="amount" placeholder="Sonuç" />
+      <input 
+        value={result} 
+        type="number" 
+        className="amount" 
+        placeholder="Sonuç"
+        readOnly
+      />
 
       <div>
-        <button className="convert-button">Convert</button>
+        <button onClick={exchange} className="convert-button">
+          Convert
+        </button>
       </div>
 
     </div>
   );
 }
 
-export default Currency
+export default Currency;
